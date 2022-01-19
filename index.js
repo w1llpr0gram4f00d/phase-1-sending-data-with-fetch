726e698d08fe606c288a0580ea003d40585f82ac
Marc Cavazos <marcavazos19@icloud.com>
2022-01-18 21:41:07 -0500

    commit tree d28391c7ebf42392b3fc251d701452059ef08ec0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+//const { minCalls } = require("sinon/lib/sinon/mock-expectation")
+
 // Add your code here
+const submitData = (name, email) => {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        let id = document.createElement('p')
+        let body = document.querySelector('body');
+        id.innerText = data.id
+        body.append(id)
+    })
+    // .catch((error) => {
+
+    // })
+}
+
